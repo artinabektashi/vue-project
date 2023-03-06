0ae248db443a2fa1fe2c7f91b22dbe57c177dcb2
Ledejna <ledejnasalihu4@gmail.com>
2023-03-06 21:10:26 +0100

    mongodb connection

--- book-api/server.js
+++ book-api/server.js
@@ -1,34 +1,37 @@
 // const express = require('express');
 import express from 'express';
-import {ReasonPhrases, StatusCodes} from 'http-status-codes';
+import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import categoriesRouter from './routes/categoriesRouter';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
-const PORT = 3000;
-const app = express();
+mongoose.connect("mongodb://localhost:27017").then(() => {
+    console.log("Connected to mongodb");
 
-app.use(
-    cors({
-        origin: 'http://localhost:8080',
+    const PORT = 3000;
+
+    const app = express();
+
+    app.use(
+        cors({
+            origin: 'http://localhost:8080',
+        })
+    )
+
+    app.use(express.urlencoded({ extended: true }))
+    app.use(express.json());
+
+    app.get('/user', (req, res) => {
+        return res.json({ name: 'John', age: 21 })
     })
-)
 
-app.use(express.urlencoded({extended:true}))
-app.use(express.json());
+    app.use('/category', categoriesRouter);
 
+    app.get('*', (req, res) => {
+        return res.status(StatusCodes.NOT_FOUND).json({ error: ReasonPhrases.NOT_FOUND })
+    })
 
-
-app.get('/user', (req,res) =>{
-    return res.json({name:'John', age:21})
-})
-
-app.use('/category', categoriesRouter);
-
-
-app.get('*',(req,res) =>{
-    return res.status(StatusCodes.NOT_FOUND).json({error: ReasonPhrases.NOT_FOUND})
-})
-
-app.listen(PORT, () =>{
-    console.log(`api port ${PORT}`)
+    app.listen(PORT, () => {
+        console.log(`api port ${PORT}`)
+    })
 })
